af9bcb089102ba9ed7e8a6e058426a66fa640a60
fractiunate <fractiunate@gmail.com>
2022-11-14 12:00:25 +0100

    add swagger ui bootstrap

--- index.js
+++ index.js
@@ -9,9 +9,67 @@ fastify.register(require("fastify-mongodb"), {
 });
 fastify.register(require("./routes/users"));
 
-fastify.get("/", function (req, reply) {
-  reply.send({ message: "Hello! Go to /users instead" });
-});
+// fastify.get("/", function (req, reply) {
+//   reply.send({ message: "Hello! Go to /users instead" });
+// });
+
+fastify.register(require('@fastify/swagger'), {
+  swagger: {
+    info: {
+      title: 'Test swagger',
+      description: 'Testing the Fastify swagger API',
+      version: '0.1.0'
+    },
+    externalDocs: {
+      url: 'https://swagger.io',
+      description: 'Find more info here'
+    },
+    host: 'localhost',
+    schemes: ['http'],
+    consumes: ['application/json'],
+    produces: ['application/json'],
+    tags: [
+      { name: 'user', description: 'User related end-points' },
+      { name: 'code', description: 'Code related end-points' }
+    ],
+    definitions: {
+      // TODO: how to get(=import) schemas? Where to store schemas?
+      User: {
+        type: 'object',
+        required: ['id', 'email'],
+        properties: {
+          id: { type: 'string', format: 'uuid' },
+          firstName: { type: 'string' },
+          lastName: { type: 'string' },
+          email: { type: 'string', format: 'email' }
+        }
+      }
+    },
+    securityDefinitions: {
+      apiKey: {
+        type: 'apiKey',
+        name: 'apiKey',
+        in: 'header'
+      }
+    }
+  }
+})
+
+fastify.register(require('@fastify/swagger-ui'), {
+  routePrefix: '/documentation',
+  uiConfig: {
+    docExpansion: 'full',
+    deepLinking: false
+  },
+  uiHooks: {
+    onRequest: function (request, reply, next) { next() },
+    preHandler: function (request, reply, next) { next() }
+  },
+  staticCSP: true,
+  transformStaticCSP: (header) => header,
+  transformSpecification: (swaggerObject, request, reply) => { return swaggerObject },
+  transformSpecificationClone: true
+})
 
 fastify.listen(8080, function (err, address) {
   if (err) {
@@ -20,3 +78,5 @@ fastify.listen(8080, function (err, address) {
   }
   fastify.log.info(`server listening on ${address}`);
 });
+
+
